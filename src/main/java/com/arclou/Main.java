374587c34333483e5e63d620bb89ce15227e69ad
Vinicius Pereira <vinicius.pereira@amadomaker.com.br>
2026-09-11 15:48:17 -0300

    feat: Adiciona menu interativo via cli

--- src/main/java/com/arclou/Main.java
+++ src/main/java/com/arclou/Main.java
@@ -1,7 +1,41 @@
 package com.arclou;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Arclou iniciado!");
+        Scanner sc = new Scanner(System.in);
+
+        int option = 0;
+
+        do {
+            System.out.println("========================");
+            System.out.println("        ARCLOU          ");
+            System.out.println("========================");
+            System.out.println(" ");
+            System.out.println("1 - Criar evento");
+            System.out.println("2 - Listar eventos");
+            System.out.println("3 - Buscar evento");
+            System.out.println("4 - Sair");
+            System.out.println(" ");
+
+            System.out.print("Escolha: ");
+
+            if (sc.hasNextInt()) {
+                option = sc.nextInt();
+                if (option <= 0 || option > 4) {
+                    System.out.println("Erro: As opções são somente entre 1 a 4.");
+                }
+            } else {
+                String invalidInput = sc.next();
+                System.out.println("Erro: '" + invalidInput + "' não é uma das opções acima.");
+            }
+
+            System.out.println("Em construção...");
+        } while (option != 4);
+
+        System.out.println("Obrigado!!!");
+
+        sc.close();
     }
 }
